f264e5f4cfedd58937a34a5aab48984d929a62be
Fred Bainbridge <fbainbridge@live.com>
2019-01-18 22:20:31 -0600

    cleaned menu and moved to the top

--- ClientApp/boot.ts
+++ ClientApp/boot.ts
@@ -7,8 +7,6 @@ Vue.use(VueRouter);
 
 const routes = [
     { path: '/', component: require('./components/home/home.vue.html').default },
-    { path: '/counter', component: require('./components/counter/counter.vue.html').default },
-    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html').default },
     { path: '/cards', component: require('./components/cards/cards.vue.html').default },
     { path: '/boosters', component: require('./components/boosters/boosters.vue.html').default },
     { 
--- ClientApp/components/app/app.ts
+++ ClientApp/components/app/app.ts
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 import BootstrapVue from 'bootstrap-vue'
-//import BoosterSelectionComponent from '../booster-selection/booster-selection'
+import BoosterSelectionComponent from '../booster-selection/booster-selection'
 
 Vue.use(BootstrapVue);
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 @Component({
     components: {
